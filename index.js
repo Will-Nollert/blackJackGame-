08f4615d824522233a4d7b5341a71c575307671e
WillN <will.nollert@gmail.com>
2023-04-03 14:24:17 -0500

    add: make players function and deal a hand

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 const suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 const values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
 //Function to create a deck object
+//can use the same function to create multiple decks if I want to add the option to play with multiple decks
 function createDeck() {
     let deck = [];
     for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
@@ -18,6 +19,7 @@ function createDeck() {
 }
 
 //Function to shuffle the deck of cards
+//can use the same function to shuffle multiple decks if I want to add the option to play with multiple decks
 function shuffleDeck(deck){
     for(let i=0; i<deck.length; i++){
         let swapIdx = Math.trunc(Math.random()*deck.length);
@@ -30,4 +32,49 @@ function shuffleDeck(deck){
 //log the shuffled deck
 let deck = createDeck();
 shuffleDeck(deck);
-console.log(deck);
+//console.log(deck);
+
+
+//create allPlayers array
+let allPlayers = [];
+
+//create a dealer object
+let dealer = {  
+    name: 'Dealer',
+    hand: [],
+    score: 0,
+}
+//push the dealer to the allPlayers object
+allPlayers.push(dealer);
+
+//create a function to make a new player object     
+function makePlayer(){
+    let player = {
+        name: "player " + (allPlayers.length),
+        hand: [],
+        score: 0,
+    }
+    //push the new player to the allPlayers object
+    allPlayers.push(player);
+}
+
+
+//make 5 nwe players and log the allPlayers object
+makePlayer();
+makePlayer();
+makePlayer();
+makePlayer();
+makePlayer();
+
+
+//deal cards to the dealer and players and log the result to the console
+function dealCards(){
+    for(let i=0; i<2; i++){
+        for(let j=0; j<allPlayers.length; j++){
+            allPlayers[j].hand.push(deck.pop());
+        }
+    }
+}
+dealCards();
+console.log(allPlayers);
+
